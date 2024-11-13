1fe0c56859ba1484ac3d6d6cbd94ae22e63febf7
Ansar Ali <ansar@amharctech.com>
2024-11-13 17:14:59 +0600

    description removed

--- src/component/layout/index.js
+++ src/component/layout/index.js
@@ -32,7 +32,6 @@ export const GeneralLayout = ({ children }) => {
         <>
             <Head>
                 <title>iMe</title>
-                <meta name="description" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
